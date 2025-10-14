91076310f924bac4fd6a9f509677a309ee56a43f
Erick Abreu <erickwgr21@gmail.com>
2025-10-14 11:54:15 -0300

    Limpando importações

--- dart/meu_app_inicial/bin/meu_app_inicial.dart
+++ dart/meu_app_inicial/bin/meu_app_inicial.dart
@@ -1,5 +1,3 @@
-import 'package:meu_app_inicial/meu_app_inicial.dart' as meu_app_inicial;
-
-void main(List<String> arguments) {
-  print('Hello world: ${meu_app_inicial.calculate()}!');
+void main() {
+  print('Hello, World!');  
 }
