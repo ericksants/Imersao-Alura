28be2ae8d63913e3effe464fb688acd5b854748d
Erick Abreu <erickwgr21@gmail.com>
2025-11-10 19:36:03 -0300

    Trabalhando com Services

---
+++ dart/dart_assincronismo/lib/services/account_service.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:main/api_key.dart';
+import 'package:main/models/account.dart';
+
+class AccountService {
+  final StreamController<String> _streamController = StreamController<String>();
+  Stream<String> get streamInfos => _streamController.stream;
+
+  String url = 'https://api.github.com/gists/56161ef65fa6d4852b25fa244e31bddf';
+
+  Future<List<Account>> getAll() async {
+    Response response = await get(Uri.parse(url));
+    _streamController.add("${DateTime.now()} | Requisição Assíncrona");
+
+    Map<String, dynamic> mapResponse = json.decode(response.body);
+    List<dynamic> listDynamic = json.decode(
+      mapResponse["files"]["accounts.json"]["content"],
+    );
+
+    List<Account> listAccounts = [];
+
+    for (var dyn in listDynamic) {
+      Map<String, dynamic> mapAccount = dyn as Map<String, dynamic>;
+      Account account = Account.fromMap(mapAccount);
+      listAccounts.add(account);
+    }
+    return listAccounts;
+  }
+
+  Future addAccount(Account account) async {
+    List<Account> listAccounts = await getAll();
+    listAccounts.add(account);
+
+    List<Map<String, dynamic>> listContent = [];
+    for (Account account in listAccounts) {
+      listContent.add(account.toMap());
+    }
+
+    String content = json.encode(listContent);
+
+    Response response = await post(
+      Uri.parse(url),
+      headers: {'Authorization': 'Bearer $apiKey'},
+      body: jsonEncode({
+        "description": "accounts.json",
+        "public": true,
+        "files": {
+          "accounts.json": {"content": content},
+        },
+      }),
+    );
+
+    if (response.statusCode.toString()[0] == "2") {
+      _streamController.add(
+        "${DateTime.now()} | Requisição adição bem sucedida (${account.name}).",
+      );
+    } else {
+      _streamController.add(
+        "${DateTime.now()} | Requisição falhou (${account.name}).",
+      );
+    }
+  }
+}
