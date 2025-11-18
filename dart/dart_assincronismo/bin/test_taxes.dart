bccedabcf846bb0928489d1bc40a0f48b2cb7178
Erick Abreu <erickwgr21@gmail.com>
2025-11-18 16:13:57 -0300

    Teste unitarios

---
+++ dart/dart_assincronismo/bin/test_taxes.dart
@@ -0,0 +1,39 @@
+import 'package:main/models/account.dart';
+import 'package:main/helpers/helper_taxes.dart';
+
+void main() {
+  final aAmb = Account(
+    id: '1',
+    name: 'Ana',
+    lastName: 'A',
+    balance: 10000.0,
+    accountType: 'Ambrosia',
+  );
+  final aCan = Account(
+    id: '2',
+    name: 'Beto',
+    lastName: 'B',
+    balance: 10000.0,
+    accountType: 'Canjica',
+  );
+  final aPud = Account(
+    id: '3',
+    name: 'Cris',
+    lastName: 'C',
+    balance: 10000.0,
+    accountType: 'Pudim',
+  );
+  final aBrg = Account(
+    id: '4',
+    name: 'Dani',
+    lastName: 'D',
+    balance: 10000.0,
+    accountType: 'Brigadeiro',
+  );
+
+  print('Ambrosia tax (6000): ${calculateTaxesByAccount(aAmb, 6000)}');
+  print('Canjica tax (6000): ${calculateTaxesByAccount(aCan, 6000)}');
+  print('Pudim tax (6000): ${calculateTaxesByAccount(aPud, 6000)}');
+  print('Brigadeiro tax (6000): ${calculateTaxesByAccount(aBrg, 6000)}');
+  print('Below threshold (5000): ${calculateTaxesByAccount(aAmb, 5000)}');
+}
